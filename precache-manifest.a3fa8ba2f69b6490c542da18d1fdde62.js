531801e511d5b86a8c7aa351f363e9fe8e078ab5
Lukahn2501 <kevin.roy.ic@gmail.com>
2018-10-24 18:02:22 +0200

    Updates

--- precache-manifest.a3fa8ba2f69b6490c542da18d1fdde62.js
+++ precache-manifest.a3fa8ba2f69b6490c542da18d1fdde62.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/react-portfolio/static/js/runtime~main.f8926043.js"
   },
   {
-    "revision": "f41c30e6a07c17fbe2dd",
-    "url": "/react-portfolio/static/js/main.f41c30e6.chunk.js"
+    "revision": "a3eeb9c7727a497d24d5",
+    "url": "/react-portfolio/static/js/main.a3eeb9c7.chunk.js"
   },
   {
     "revision": "dc9d6e84bb67a7bebc2f",
     "url": "/react-portfolio/static/js/1.dc9d6e84.chunk.js"
   },
   {
-    "revision": "f41c30e6a07c17fbe2dd",
+    "revision": "a3eeb9c7727a497d24d5",
     "url": "/react-portfolio/static/css/main.64253227.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/react-portfolio/static/css/1.ff2d018f.chunk.css"
   },
   {
-    "revision": "01b0371415c5136b069ce3add176f53e",
+    "revision": "64940dda64bdd00e0a706ffc40c6eefc",
     "url": "/react-portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-portfolio/precache-manifest.604277a4812962e3d1f1f59224f4969d.js"
+  "/react-portfolio/precache-manifest.a3fa8ba2f69b6490c542da18d1fdde62.js"
 );
 
 workbox.clientsClaim();
