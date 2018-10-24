a5d99eba402aedc5dce75f1f1d9e67559ee09f69
Lukahn2501 <kevin.roy.ic@gmail.com>
2018-10-24 12:22:57 +0200

    FIX: assests folder location

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -7,7 +7,10 @@ class Landing extends Component {
             <div style={{width: '100%', maring: 'auto'}}>
                 <Grid className="landing-grid">
                     <Cell col={12}>
-                    <img src="assets\bw.jpg" alt="avataralt" className="avatar-img"
+                    <img 
+                    src="/assets/avatar.jpg" 
+                    alt="avataralt" 
+                    className="avatar-img"
                     />
                     </Cell>
                 </Grid>
