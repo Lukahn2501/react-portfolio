d8da69c95376b48f473b0a44c4a8fd6317ed071a
Lukahn2501 <kevin.roy.ic@gmail.com>
2018-10-24 20:09:28 +0200

    FIX: menu tweaks

--- src/App.js
+++ src/App.js
@@ -9,21 +9,24 @@ class App extends Component {
     return (
         <div className="demo-big-content">
             <Layout>
-                <Header className="header-color" title="Title" scroll > 
+                <Header className="header-color" title=" " scroll > 
                     <Navigation>
+                        <Link to="." style={{fontWeight:'bolder'}}>Home</Link>
                         <Link to="/resume">Resume</Link>
                         <Link to="/aboutme">About Me</Link>
                         <Link to="/projects">Projects</Link>
                         <Link to="/contact">Contact</Link>
                     </Navigation>
                 </Header>
-                <Drawer title="Title">
+                <Drawer>
                     <Navigation>
+                        <Link to="." style={{fontWeight:'bolder'}}>Home</Link>
                         <Link to="/resume">Resume</Link>
                         <Link to="/aboutme">About Me</Link>
                         <Link to="/projects">Projects</Link>
                         <Link to="/contact">Contact</Link>
                     </Navigation>
+                    <p style={{color:'black', marginLeft:'20px', marginRight:'20px', marginTop:'20px'}}>This site is used as a discovery React Project. It uses ReactMDL and is hosted on GitHub Pages</p>
                 </Drawer>
                 <Content>
                     <div className="page-content" />
