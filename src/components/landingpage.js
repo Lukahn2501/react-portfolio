9652388b9a1ff96c0839755ccfc307daf6b1e403
Lukahn2501 <kevin.roy.ic@gmail.com>
2018-10-24 09:50:16 +0200

    FIX: design changes TODO: fix relative path img

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
-
+import { Grid, Cell } from 'react-mdl';
 
 class Landing extends Component {
     render() {
         return(
-            <div><h1>Landing Page</h1></div>
+            <div style={{width: '100%', maring: 'auto'}}>
+                <Grid className="landing-grid">
+                    <Cell col={12}>
+                    <img src="assets\bw.jpg" alt="avataralt" className="avatar-img"
+                    />
+                    </Cell>
+                </Grid>
+            </div>
         )
     }
 }
